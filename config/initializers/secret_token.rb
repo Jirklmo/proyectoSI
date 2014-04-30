040be3d8d65e589ec07c835efba7e37c8a12daff
Jorge Riquelme <jirklmo@live.com.ar>
2014-04-30 09:59:18 -0500

    Se  subio cosa

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProyectoSI::Application.config.secret_key_base = 'd6a5a436036683c01ba2f910fd74fba7a2e957f7220362280940adb87b9f05d76c6121506364e301fb86ed83248773adafdf6f74ce783ca8b592b714ac6fc66f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProyectoSI::Application.config.session_store :cookie_store, key: '_ProyectoSI_session'
